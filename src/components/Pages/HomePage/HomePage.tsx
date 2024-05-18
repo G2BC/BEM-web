dcd607c0d15d445f8316ff49df5b82b7e41528b0
Pedro-Benevides <pvsbenevides197@gmail.com>
2024-05-17 23:41:18 -0300

    feat(HomePage): implementa mapa de calor

--- src/components/Pages/HomePage/HomePage.tsx
+++ src/components/Pages/HomePage/HomePage.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import FungiService from "../../../services/FungiService";
+import HeatMap from "../../HeatMap/HeatMap";
 
 const HomePage: React.FC = () => {
     const fungiService: FungiService = new FungiService();
-    const [fungisHeatMap, setFungisHeatMap] = useState<any>([]);
+    const [fungisHeatMap, setFungisHeatMap] = useState<any>();
 
     useEffect(() => {
         getFungisForMap();
@@ -12,18 +13,32 @@ const HomePage: React.FC = () => {
     const getFungisForMap = async () => {
         let data = await fungiService.getForHeatMap();
         if (data) {
+            data = Object.keys(data).map((stateAc) => {
+
+                return { state: stateAc, intensity: data[stateAc].occurrences_count }
+            })
             setFungisHeatMap(data);
 
         }
-
-        return data;
     };
 
+    const heatData = [
+        { state: 'SP', intensity: 0.5 },
+        { state: 'RJ', intensity: 0.4 },
+        { state: 'DF', intensity: 0.3 },
+        // Adicione mais dados conforme necessário
+    ];
 
     return (
-        <div>
-            <h1>Mapa de calor</h1>
-        </div>
+        <>
+            <div>
+                <h1>Mapa de Calor BEM</h1>
+                {fungisHeatMap != undefined ?
+                    <HeatMap data={fungisHeatMap} />
+                    : <></>
+                }
+            </div>
+        </>
     )
 }
 
