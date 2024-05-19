92cda911fb7d071fc8328f942763ad8294771d94
OokamyAbyss474 <102814036+ookamyabyss@users.noreply.github.com>
2024-05-19 05:12:42 -0300

    update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HeaderContainer, LogoImage, HeadList, HeadItem, HeadLink } from './Header.styles';
+import { HeaderContainer, LogoImage, HeadList, HeadItem, HeadLink, HeadButton1, HeadButton2} from './Header.styles';
 // import Button from '../Utils/Button/Button';
 import Logo from '../../assets/cogumelo.png';
 import SearchArea from '../Utils/SearchArea/SearchArea';
@@ -9,22 +9,26 @@ const Header: React.FC = () => {
   return (
     <HeaderContainer>
       <HeadList>
+
         <HeadItem className="head-logo">
           <LogoImage src={Logo} alt="Logo" />
         </HeadItem>
-        <HeadItem className="head-item">
+
+        <HeadItem>
           <HeadLink href="/">BEM</HeadLink>
         </HeadItem>
-        <HeadItem>
+
+        <HeadItem style={{ flex: 1, justifyContent: 'center' }}>
             <SearchArea/>
         </HeadItem>
-        <HeadItem style={{ marginLeft: 'auto' }}>
-          <Button variant='contained'>Entrar</Button>
-        </HeadItem>
-        <HeadItem>
-          <Button variant="outlined">Cadastrar</Button>
+
+        <HeadItem className="head-buttons" style={{ justifyContent: 'flex-end' }}>
+          <HeadButton2>Entrar</HeadButton2>
+          <HeadButton1>Cadastrar</HeadButton1>  
         </HeadItem>
       </HeadList>
+      
+      
     </HeaderContainer>
   );
 };
