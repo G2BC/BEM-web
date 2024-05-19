4c4630830e155cf32eb4376c64148666bf5dbd16
OokamyAbyss474 <102814036+ookamyabyss@users.noreply.github.com>
2024-05-19 01:18:01 -0300

    add estlização

--- src/components/Footer/Footer.styles.ts
+++ src/components/Footer/Footer.styles.ts
@@ -1,86 +1,135 @@
 import styled from 'styled-components';
 
 export const FooterContainer = styled.footer`
-  background-color: #131313;
-  color: #ffffff;
-  padding: 10px 20px;
-  margin-top: 400px; /* Adicionando margem superior para distanciar do conteúdo acima */
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  background: #151414;
+  position: relative;
+  width: 100%;
 `;
 
 export const FooterContent = styled.div`
   display: flex;
   justify-content: space-between;
-  width: 80%;
-  margin: 0 auto;
-  text-align: left;
+  align-items: center;
+  padding: 20px;
+  width: 100%;
 `;
 
 export const LogoFooter = styled.div`
   flex: 40%;
 `;
 
-export const FooterSection = styled.div`
-  flex: 60%;
-  display: flex;
-`;
-
-export const FooterHeading = styled.h2`
-  font-size: 2rem;
-  margin-bottom: 10px;
-`;
-
 export const BigLogo = styled.h2`
   font-size: 3rem;
-  display: inline;
+  color: white;
+  text-align: center;
+  position: relative;
+  display: inline-block;
+
+  text-shadow:
+    1px 1px 0 #ff5e14,
+    2px 2px 0 #ff5e14,
+    3px 3px 0 #ff5e14,
+    4px 4px 0 #ff5e14;
+
+  span {
+    display: block;
+  }
+`;
+
+export const FooterLogo = styled.div`
+  flex: 1;
 `;
 
 export const FooterImage = styled.img`
-  width: 50px; /* Ajuste conforme necessário */
+  max-width: 200px;
 `;
+
+export const FooterSection = styled.div`
+  flex: 1;
+`;
+
+export const FooterHeading = styled.h3`
+  color: #fff;
+  font-size: 20px;
+  font-weight: 600;
+
+  position: relative;
+  font-size: 1.5rem;
+  margin-bottom: 20px;
+
+  &::after {
+    content: '';
+    display: block;
+    width: 40px;
+    height: 3px;
+    background-color: #ff5e14;
+    position: absolute;
+    left: 0;
+    bottom: -5px;
+  }
+`;
+
 
 export const FooterList = styled.ul`
   list-style: none;
-  padding: 30px;
-  margin: 0;
-  width: 50%;
+  margin-right: 112px;
+  padding: 0;
+  text-align: left;
 `;
 
 export const FooterListItem = styled.li`
-  margin-bottom: 25px;
+  margin-bottom: 12px;
 `;
 
 export const FooterLink = styled.a`
-  color: #ffffff;
-  text-decoration: none;
+  color: white;
+  text-transform: capitalize;
 
   &:hover {
-    color: #a65f3e;
-    text-decoration: underline; /* Opcional: adicionar sublinhado ao fazer hover */
+    color: #ff5e14;
   }
-
 `;
 
-export const JunteseSection = styled.div`
-  width: 50%;
+export const JoinUsSection = styled.div`
+  flex: 1;
+  text-align: left;
+  margin-left: 112px;
+  margin-bottom: 80px;
+  white-space: pre-wrap;
+
+  p {
+    color: white; /* Define a cor branca para o texto */
+    font-size: 16px;
+    max-width: 400px;
+    white-space: pre-wrap;
+    
+  }
 `;
 
 export const FooterBottom = styled.div`
-  margin-left: 80px;
-  text-align: left;
-  font-size: 0.8rem;
+  background: #202020;
+  padding: 25px 0;
+  width: 100%;
+  color: white;
 `;
 
 export const FooterButton = styled.button`
-background-color: #a65f3e;
-color: #ffffff;
-border: none;
-padding: 10px 20px;
-font-size: 1rem;
-cursor: pointer;
-transition: background-color 0.3s ease;
-border-radius: 5px;
+  background: #ff5e14;
+  padding: 13px 20px;
+  border: 1px solid #ff5e14;
+  color: #fff;
+  font-size: 16px;
+  font-weight: bold;
+  cursor: pointer;
+  transition: background-color 0.3s ease, color 0.3s ease;
 
-&:hover {
-  background-color: #9D8D8F;
-}
+  &:hover {
+    background: #ffffff;
+    color: #ff5e14;
+  }
 `;
+
+
