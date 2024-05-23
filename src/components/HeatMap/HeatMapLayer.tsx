7001d9194d3a28004dd42b294df7ea03aea6e645
Pedro-Benevides <pvsbenevides197@gmail.com>
2024-05-22 22:24:20 -0300

    Refact(heatMap): refatora componente para uso de hook use Map

---
+++ src/components/HeatMap/HeatMapLayer.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import * as L from 'leaflet';
+import 'leaflet.heat'; // Certifique-se de que este import funciona após a declaração do módulo.
+
+
+interface HeatMapLayerProps {
+    data: [number, number, number][]
+}
+
+const HeatMapLayer: React.FC<HeatMapLayerProps> = ({ data }) => {
+    const map = useMap();
+
+    useEffect(() => {
+        // @ts-ignore
+        const heatLayer = L.heatLayer(data, {
+            radius: 35,     // Aumenta o raio dos pontos
+            blur: 15,       // Reduz o desfoque para uma marcação mais nítida
+            max: 1.0,       // Define a intensidade máxima
+            minOpacity: 0.5, // Aumenta a opacidade mínima para tornar as áreas de baixa intensidade mais visíveis
+            gradient: {     // Define um gradiente de cores personalizado
+                0.4: 'blue',
+                0.6: 'lime',
+                0.8: 'red'
+            }
+        }).addTo(map);
+
+        return () => {
+            map.removeLayer(heatLayer);
+        };
+    }, [data, map])
+
+    return null;
+};
+
+export default HeatMapLayer;
