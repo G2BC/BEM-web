3e9e1f0e92900603dd687bb3f59658f737ff2255
Pedro-Benevides <pvsbenevides197@gmail.com>
2024-06-14 08:16:09 -0300

    retoma funcionalidade home page

--- src/components/Pages/HomePage/HomePage.tsx
+++ src/components/Pages/HomePage/HomePage.tsx
@@ -1,58 +1,87 @@
-import React, { useEffect, useState } from "react";
-import { MapContainer, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import { useEffect, useState } from "react";
+import FungiService from "../../../services/FungiService";
+import { Grid } from "@mui/material";
 import BemSubtitle from "../../Classifications/BemSubtitle";
-import HeatMapLayer from "../../HeatMap/HeatMapLayer"; // Importe o componente HeatMapLayer
+import HeatMap from "../../HeatMap/HeatMapLayer";
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet.heat'; // Certifique-se de que este import funciona após a declaração do módulo.
 import stateCoordinates from '../../../Utils/StateCoordinates';
+import HeatMapLayer from "../../HeatMap/HeatMapLayer";
 
 const HomePage: React.FC = () => {
-    const classificationFilter = (bemName: string) => {
-        // Implemente a lógica de filtragem aqui
-    };
-
-    // Dados de amostra para o mapa de calor
+    const fungiService: FungiService = new FungiService();
     const [fungisHeatMap, setFungisHeatMap] = useState<[number, number, number][]>();
+    const [fungisOccurrencesCount, setFungisOccurrencesCount] = useState<any>();
 
     useEffect(() => {
-        // Simulação de dados para o mapa de calor
-        const sampleData: { state: string, intensity: number }[] = [
-            { state: 'AC', intensity: 10 },
-            { state: 'AL', intensity: 20 },
-            // Adicione mais dados aqui conforme necessário
-        ];
+        getFungisForMap();
+    }, [])
 
-        const formattedData = formatHeatData(sampleData);
-        setFungisHeatMap(formattedData);
-    }, []);
+    const getFungisForMap = async () => {
+        let data = await fungiService.getForHeatMap();
+        if (data) {
+            setFungisOccurrencesCount(data);
 
-    // Formatar dados para o mapa de calor
+            data = Object.keys(data).map((stateAc) => {
+
+                return { state: stateAc, intensity: data[stateAc].occurrences_count }
+            });
+
+            setFungisHeatMap(formatHeatData(data));
+        }
+    };
+
+    const classificationFilter = (bemName: string) => {
+        if (fungisOccurrencesCount) {
+
+            let filteredKeys = Object.keys(fungisOccurrencesCount).filter((stateAc: string) => {
+                return fungisOccurrencesCount[stateAc].classifications_count[bemName] > 0;
+            });
+
+            let filteredMushrooms = filteredKeys.map((key) => {
+
+                return { state: key, intensity: fungisOccurrencesCount[key].classifications_count[bemName] }
+            });
+
+            setFungisHeatMap(formatHeatData(filteredMushrooms))
+        }
+    }
+
     const formatHeatData = (data: { state: string, intensity: number }[]): [number, number, number][] => {
         return data.map(({ state, intensity }) => {
-            const coords = stateCoordinates[state];
+            let coords = stateCoordinates[state];
+
             return [coords[0], coords[1], intensity] as [number, number, number];
         });
-    };
+    }
 
     return (
         <div style={{ display: 'flex', height: '100vh', overflow: 'hidden' }}>
-            {/* Mapa */}
-            <div style={{ flex: 1 }}>
-                <MapContainer center={[-14.235004, -51.92528]} zoom={4} style={{ height: '100%', width: '100%' }}>
-                    <TileLayer
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    />
-                    {/* Adicione a camada de mapa de calor */}
-                    {fungisHeatMap && <HeatMapLayer data={fungisHeatMap} />}
-                </MapContainer>
-            </div>
-            
-            {/* Tabela de Classificação */}
-            <div style={{ width: '320px', backgroundColor: '#fff', zIndex: 1, boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)', overflowY: 'auto' }}>
-                <BemSubtitle filter={classificationFilter} />
-            </div>
+            <h1>Mapa de Calor BEM</h1>
+            {fungisHeatMap != undefined ?
+                <>
+                    <Grid container spacing={1} direction="row" justifyContent='center' alignItems='stretch'>
+                        <Grid item xs={10} marginBottom={5} style={{ flex: 1 }}>
+                            <MapContainer center={[-14.235004, -51.92528]} zoom={4} id="heatmap" style={{ height: '100%', width: '100%'}}>
+                                <TileLayer
+                                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                />
+                                <HeatMapLayer data={fungisHeatMap} />
+                            </MapContainer>
+                        </Grid>
+                        <Grid item xs={2} marginBottom={7} style={{ width: '320px', backgroundColor: '#fff', zIndex: 1, boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)', overflowY: 'auto' }}>
+                            <BemSubtitle filter={classificationFilter} />
+                        </Grid>
+                    </Grid>
+                </>
+                :
+                <>
+                </>
+            }
         </div>
-    );
-};
+    )
+}
 
 export default HomePage;
