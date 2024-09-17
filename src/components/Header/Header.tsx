2b5e74f461ad6cdc6a34ae516892740aec0e9feb
wilyJ80 <abjurandam@gmail.com>
2024-09-17 17:52:02 -0300

    Feat: mudancas na pagina inicial

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HeaderContainer, LogoImage, HeadList, HeadItem, HeadLink, HeadButtonEnter, HeadButtonRegister } from './Header.styles';
+import { HeaderContainer, LogoImage, HeadList, HeadItem, HeadLink, HeadButtonEnter, HeadButtonRegister, NormalHeadText } from './Header.styles';
 import Logo from '../../assets/cogumelo.png';
 import SearchArea from '../Utils/SearchArea/SearchArea';
 
@@ -16,12 +16,44 @@ const Header: React.FC = () => {
         </HeadItem>
 
         <HeadItem>
+          <NormalHeadText href="/">Início</NormalHeadText>
+        </HeadItem>
+
+        <HeadItem>
+          <NormalHeadText href="/">Explorar</NormalHeadText>
+        </HeadItem>
+
+        <HeadItem>
+          <NormalHeadText href="/">Como citar</NormalHeadText>
+        </HeadItem>
+
+        <HeadItem>
+          <NormalHeadText href="/">Sobre</NormalHeadText>
+        </HeadItem>
+
+        <HeadItem>
+          <NormalHeadText href="/">Instruções</NormalHeadText>
+        </HeadItem>
+
+        <HeadItem>
+          <NormalHeadText href="/">Contato</NormalHeadText>
+        </HeadItem>
+
+        <HeadItem>
+          <NormalHeadText href="/collaborators">Colaboradores</NormalHeadText>
+        </HeadItem>
+
+        <HeadItem>
           <SearchArea />
         </HeadItem>
 
         <HeadItem className="head-buttons">
-          <HeadButtonEnter>Entrar</HeadButtonEnter>
-          <HeadButtonRegister>Cadastrar</HeadButtonRegister>
+          <HeadButtonEnter>
+            Entrar
+          </HeadButtonEnter>
+          <HeadButtonRegister>
+            Cadastrar
+          </HeadButtonRegister>
         </HeadItem>
       </HeadList>
     </HeaderContainer>
