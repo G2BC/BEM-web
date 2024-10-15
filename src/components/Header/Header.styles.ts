02d1643064f6dc8114df641db797adb250aff87f
wilyJ80 <abjurandam@gmail.com>
2024-10-15 18:14:35 -0300

    feat: reformular header

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -10,9 +10,13 @@ export const HeaderContainer = styled.nav`
 `;
 
 export const LogoImage = styled.img`
-  width: 50px;
-  height: 50px;
+  width: 100%;
+  height: 100%;
   padding-right: 13px; /* Espaço à direita */
+
+  @media (max-width: 768px) {
+    width: 70%;
+  }
 `;
 
 export const HeadList = styled.ul`
@@ -20,16 +24,15 @@ export const HeadList = styled.ul`
   display: flex;
   align-items: center;
   justify-content: space-between; /* Ajusta a distribuição dos itens */
-  padding: 0 20px; /* Ajuste do padding para um melhor espaçamento */
+  padding: 10px 90px; /* Ajuste do padding para um melhor espaçamento */
   margin: 0; /* Remove margem para evitar overflow */
   flex-wrap: wrap; /* Permite que os itens do cabeçalho se envolvam em várias linhas, se necessário */
   width: 100%; /* Garante que a lista ocupe 100% da largura */
   box-sizing: border-box; /* Inclui padding e border na largura e altura do elemento */
 
   @media (max-width: 768px) {
-    justify-content: center; /* Centraliza os itens em telas menores */
-    flex-direction: column; /* Altera a direção da lista para exibir os itens verticalmente */
-    align-items: flex-start; /* Ajusta a alinhamento dos itens para o início */
+    justify-content: space-between;
+    align-items: center;
     padding: 10px; /* Ajusta o padding para um melhor espaçamento em telas menores */
   }
 `;
@@ -44,9 +47,8 @@ export const HeadItem = styled.div`
   }
 
   @media (max-width: 768px) {
-    width: 100%;
-    justify-content: space-between;
-    margin-bottom: 10px;
+    justify-content: flex-start;
+    padding: 10px;
   }
 `;
 
@@ -121,3 +123,43 @@ export const NormalHeadText = styled.a({
     color: '#ff5e14',
   }
 });
+
+export const HamburgerMenu = styled.div`
+  display: none;
+  font-size: 90px;
+  cursor: pointer;
+  color: white;
+  top: 10px;
+  right: 20px;
+
+  @media (max-width: 768px) {
+    display: block;
+  }
+`;
+
+export const MobileMenu = styled.div<{ isOpen: boolean }>`
+  display: ${({ isOpen }) => (isOpen ? 'flex' : 'none')};
+  flex-direction: column;
+  align-items: flex-start;
+  background-color: #131313;
+  width: 100%;
+  top: 60px; /* Adjust based on your header height */
+  left: 0;
+  z-index: 100;
+
+  @media (min-width: 769px) {
+    display: none;
+  }
+`;
+
+export const LinkContainer = styled.div`
+@media (max-width: 768px){
+display: none;
+}
+`;
+
+export const MainButtonContainer = styled.div`
+@media (max-width: 768px){
+display: none;
+}
+`;
